9a16f674e4d714c5f272fd3fb6c2da6fcbf14fd2
Alan castro <124410934+Flainxps@users.noreply.github.com>
2023-05-29 16:53:40 -0700

    Add files via upload

---
+++ Parcial3/practica 1 3p.php
@@ -0,0 +1,21 @@
+<?php
+
+    echo "<h1>Hola Mundo</h1>";
+    
+    echo "<hr>";
+
+    $nombre = "eso";
+    $edad = 16;
+
+    if($edad >= 18){
+        echo $nombre . "woooo";
+    }else{
+        echo $nombre . " wooooooo";
+    }
+
+    echo "<br><br>";
+
+    for($i=1; $i<=10; $i++){
+        echo $i . "<br>";
+    }
+?> 
